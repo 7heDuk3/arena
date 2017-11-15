221ba87b420e13da1aa951ae237c367e71e02611
Mike Persson <mike.persson@etteplan.com>
2017-11-15 15:46:34 +0100

    Added mouse steering to character movement. Camera slurp is a little laggy

--- Assets/mmoCharacterController.cs
+++ Assets/mmoCharacterController.cs
@@ -8,6 +8,7 @@ public class mmoCharacterController : MonoBehaviour {
 
     private float mouseX, mouseY;
     private bool mouseDownL = false;
+    private bool mouseDownR = false;
     public float mouseSensitivity = 5f;
     public float mouseYPosition = 1f;
 
@@ -22,6 +23,7 @@ public class mmoCharacterController : MonoBehaviour {
     public float zoomMax = -10f;
 
     public float rotationSpeed = 5f;
+    public float slerpSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
@@ -38,68 +40,98 @@ public class mmoCharacterController : MonoBehaviour {
         if (zoom < zoomMax)
             zoom = zoomMax;
 
-        //playerCam.transform.localPosition = new Vector3(0, 0, zoom);
-
-        if (Input.GetMouseButton (0))
+        if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
         {
             mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
             mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-            mouseDownL = true;
+
+            mouseY = Mathf.Clamp(mouseY, -60f, 60f);
+
+            centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+            Vector3 charMovement = new Vector3(0, 0, moveSpeed);
+            charMovement = character.rotation * charMovement;
+            character.GetComponent<CharacterController>().Move(charMovement * Time.deltaTime);
+
+            centerPoint.localPosition = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
+
+            character.localRotation = Quaternion.Euler(0, centerPoint.eulerAngles.y, 0);
         }
         else
         {
-            mouseX = centerPoint.eulerAngles.y;
-            mouseY = centerPoint.eulerAngles.x;
-            mouseDownL = false;
-        }
-
-        mouseY = Mathf.Clamp(mouseY, -60f, 60f);
-        //playerCam.LookAt(centerPoint);
-        if (mouseDownL)
-        {
-            centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            character.localRotation = Quaternion.Euler(0, character.eulerAngles.y - rotationSpeed, 0);
-            //centerPoint.rotation = Quaternion.Euler(0, centerPoint.eulerAngles.y - rotationSpeed, 0);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            character.localRotation = Quaternion.Euler(0, character.eulerAngles.y + rotationSpeed, 0);
-            //centerPoint.rotation = Quaternion.Euler(0, character.eulerAngles.y, 0);
-        }
-
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            Vector3 movement = new Vector3(0, 0, moveSpeed);
-            Vector3 charMovement = character.rotation * movement;
-            Vector3 centerPMovement = centerPoint.rotation * movement;
-
-            character.GetComponent<CharacterController>().Move(charMovement * Time.deltaTime);
-            centerPoint.localPosition = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
-
-            //centerPoint.rotation = Quaternion.Euler(0, centerPoint.eulerAngles.y - rotationSpeed, 0);
-            Quaternion turnAngle = Quaternion.Euler(0, character.eulerAngles.y, 0);
-            if (!mouseDownL)
+            if (Input.GetMouseButton(0))
             {
-                //centerPoint.position = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
-                centerPoint.rotation = Quaternion.Slerp(centerPoint.rotation, character.rotation, Time.deltaTime * rotationSpeed);
+                mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
+                mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+                mouseDownL = true;
+                mouseDownR = false;
             }
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            Vector3 movement = new Vector3(0, 0, -moveSpeed * moveBackMultiplier);
-            movement = character.rotation * movement;
-
-            character.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
-            centerPoint.position = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
-
-            if (!mouseDownL)
+            else if (Input.GetMouseButton(1))
             {
-                centerPoint.rotation = Quaternion.Slerp(centerPoint.rotation, character.rotation, /*Time.deltaTime **/ rotationSpeed);
+                mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
+                mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+                mouseDownL = false;
+                mouseDownR = true;
+            }
+            else
+            {
+                mouseX = centerPoint.eulerAngles.y;
+                mouseY = centerPoint.eulerAngles.x;
+                mouseDownL = false;
+                mouseDownR = false;
+            }
+
+            mouseY = Mathf.Clamp(mouseY, -60f, 60f);
+            
+            if (mouseDownL || mouseDownR)
+            {
+                centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
+            }
+
+            if (!mouseDownR)
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    character.localRotation = Quaternion.Euler(0, character.eulerAngles.y - rotationSpeed, 0);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    character.localRotation = Quaternion.Euler(0, character.eulerAngles.y + rotationSpeed, 0);
+                }
+            }
+            else
+            {
+                character.localRotation = Quaternion.Euler(0, centerPoint.eulerAngles.y, 0);
+            }
+
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                Vector3 movement = new Vector3(0, 0, moveSpeed);
+                Vector3 charMovement = character.rotation * movement;
+                Vector3 centerPMovement = centerPoint.rotation * movement;
+
+                character.GetComponent<CharacterController>().Move(charMovement * Time.deltaTime);
+                centerPoint.localPosition = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
+
+                
+                if (!mouseDownL && !mouseDownR)
+                {
+                    centerPoint.rotation = Quaternion.Slerp(centerPoint.rotation, character.rotation, Time.deltaTime * rotationSpeed);
+                }
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                Vector3 movement = new Vector3(0, 0, -moveSpeed * moveBackMultiplier);
+                movement = character.rotation * movement;
+
+                character.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+                centerPoint.position = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
+
+                if (!mouseDownL && !mouseDownR)
+                {
+                    centerPoint.rotation = Quaternion.Slerp(centerPoint.rotation, character.rotation, Time.deltaTime * rotationSpeed);
+                }
             }
         }
     }
